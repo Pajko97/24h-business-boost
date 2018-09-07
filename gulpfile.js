0cc17f0136cc8ea47f7cf7fac2618b2902909a3c
Pavle Janjusevic <axeofthedead@gmail.com>
2018-09-07 19:17:36 +0200

    Working

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const minify = require('gulp-minify-css');
+const plumber = require('gulp-plumber');
 
 var input = './sass/index.scss';
 var output = './compiled';
@@ -10,10 +11,17 @@ var output = './compiled';
 gulp.task('sass', function () {
     return gulp
       .src(input)
+      .pipe(plumber())
       .pipe(sass())
       .pipe(autoprefixer('last 2 versions'))
       .pipe(minify())
       .pipe(rename('output.css'))
       .pipe(gulp.dest(output))
   });
+
+gulp.task('default', ['watch']);
+
+gulp.task('watch', function() {
+    gulp.watch('./sass/*.scss', ['sass'])
+});
 
