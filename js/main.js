92cdbfba51fa02f04cb051cc4f32ca4ecf011774
Pavle Janjusevic <axeofthedead@gmail.com>
2018-09-09 16:12:13 +0200

    Updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
+(function() {
+var config = {
+    apiKey: "AIzaSyCBQL_9bUFei73WZzFMb8JPF30TH_LONV8",
+    authDomain: "hbb-36f85.firebaseapp.com",
+    databaseURL: "https://hbb-36f85.firebaseio.com",
+    projectId: "hbb-36f85",
+    storageBucket: "",
+    messagingSenderId: "230716773049"
+};
+ firebase.initializeApp(config);
+    
+
+
 var messagesRef = firebase.database().ref('messages');
 
-document.getElementById("contactForm").addEventListener('submit', sendForm);
+document.getElementById("contact__form").addEventListener('submit', sendForm);
 
 function getInputVal(id) {
     return document.getElementById(id).value;
@@ -8,16 +21,16 @@ function getInputVal(id) {
 
 function sendForm(e) {
     e.preventDefault();
-
+    console.log('ide');
     var name= getInputVal('name-field');
     var mail = getInputVal('mail-field');
     var message = getInputVal('message-field');
 
     saveMessage(name, mail, message);
 
-  }
+}
 
-  function saveMessage(name, mail, message) {
+function saveMessage(name, mail, message) {
     let newMessageRef = messagesRef.push();
     newMessageRef.set({
         name: name,
@@ -25,3 +38,5 @@ function sendForm(e) {
         message: message
     })
 }
+
+})();
